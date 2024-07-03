cb72f4b17f2f4e56834e377a7000ed53ff69ee7c
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-03 21:54:05 +0000

    update task_0/js/main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -5,3 +5,31 @@ interface Student {
   location: string;
 }
 
+const studentOne: Student = {
+  firstName: 'John';
+  lastName: 'Doe';
+  age: 34;
+  location: 'NYC';
+}
+
+const studentTwo: Student = {
+  firstName: 'Larry';
+  lastName: 'Page';
+  age: 24;
+  location: 'WDC';
+}
+
+const studentsList: Student[] = [studentOne, studentTwo];
+
+const table = document.createElement("table");
+
+studentsList.forEach(student => {
+  const row = table.insertRow();
+  const cell_1 = row.insertCell(0);
+  const cell_2 = row.insertCell(1);
+
+  cell_1.textContent = student.firstName;
+  cell_2.textContent = student.location;
+});
+
+document.body.appendChild(table);
