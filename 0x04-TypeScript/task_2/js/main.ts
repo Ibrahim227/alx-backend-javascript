06ea09574076f78cac0ca9ed73f2dd7186dfec47
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-03 22:17:56 +0000

    update task_2js/main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -11,8 +11,39 @@ interface TeacherInterface {
 }
 
 class Director implements DirectorInterface {
-  workFromHome () {
+  workFromHome():string {
     return 'Working from home';
   }
 
-  
+  getCoffeeBreak():string {
+     return 'Getting a coffee break';
+  }
+
+  workDirectorTasks():string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome():string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak():string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks():string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Teacher | Director {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+
